1f3348d877219fd8faee7f94e003508e2bd3e9d7
professionalali22 <54819672+AliShaheen232@users.noreply.github.com>
2025-03-22 23:40:57 +0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,42 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
-import {
-  BrowserRouter as Router,
-  Route,
-Redirect
-} from "react-router-dom";
-import Bridge from "./Bridge";
+// import React, { Component } from "react";
+// import { render } from "react-dom";
+// import {
+//   BrowserRouter as Router,
+//   Route,
+// Redirect
+// } from "react-router-dom";
+// import Bridge from "./Bridge";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "React",
-      isUserAuthenticated: true
-    };
-  }
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       name: "React",
+//       isUserAuthenticated: true
+//     };
+//   }
+// // 
+//   render() {
 
-  render() {
-
-    return (
-      <div>
-        <Router>
-          <div>
-              <Route
-                exact
-                path="/"
-                render={() => {
-                    return (
-                      <Redirect to="/" /> 
-                           )
-                }}
-              />
-               <Route  path="/" component={Bridge} />
-          </div>
-        </Router>
-      </div>
-    );
-  }
-}
-export default App;
-render(<App />, document.getElementById("root"));
+//     return (
+//       <div>
+//         <Router>
+//           <div>
+//               <Route
+//                 exact
+//                 path="/"
+//                 render={() => {
+//                     return (
+//                       <Redirect to="/" /> 
+//                            )
+//                 }}
+//               />
+//                <Route  path="/" component={Bridge} />
+//           </div>
+//         </Router>
+//       </div>
+//     );
+//   }
+// }
+// export default App;
+// render(<App />, document.getElementById("root"));
