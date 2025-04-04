64666d99491fc1edfe133a53b7cc6a3d40978987
professionalali22 <54819672+AliShaheen232@users.noreply.github.com>
2025-04-05 03:20:38 +0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,35 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-import Bridge from "./Bridge";
+// import React, { Component } from "react";
+// import { render } from "react-dom";
+// import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+// import Bridge from "./Bridge";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "React",
-      isUserAuthenticated: true,
-    };
-  }
-  //
-  render() {
-    return (
-      <div>
-        <Router>
-          <div>
-            <Route
-              exact
-              path="/"
-              render={() => {
-                return <Redirect to="/" />;
-              }}
-            />
-            <Route path="/" component={Bridge} />
-          </div>
-        </Router>
-      </div>
-    );
-  }
-}
-export default App;
-render(<App />, document.getElementById("root"));
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       name: "React",
+//       isUserAuthenticated: true,
+//     };
+//   }
+//   //
+//   render() {
+//     return (
+//       <div>
+//         <Router>
+//           <div>
+//             <Route
+//               exact
+//               // path="/"
+//               render={() => {
+//                 return <Redirect to="/" />;
+//               }}
+//             />
+//             <Route path="/" component={Bridge} />
+//           </div>
+//         </Router>
+//       </div>
+//     );
+//   }
+// }
+// export default App;
+// render(<App />, document.getElementById("root"));
